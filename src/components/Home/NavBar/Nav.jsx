a260b1705fe8399b45b02a98ce226599db4c08ec
Yashasewi <Yashasewisingh2668@gmail.com>
2023-03-19 15:15:35 +0530

    firebase hosing and working everythng

--- src/components/Home/NavBar/Nav.jsx
+++ src/components/Home/NavBar/Nav.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import netflix from "./netflix.png";
+import "./nav.css";
 
 const Profile_icon = [
     "https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png",
