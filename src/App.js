c8a1a65f20549177287a9de1c5cac0e4c95e5b58
Yashasewi <Yashasewisingh2668@gmail.com>
2023-04-20 16:29:48 +0530

    added the routes and login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/Home/HomePage";
+import Login from "components/Login/Login";
 import "./App.css";
 
 function App() {
     return (
+        // router for creating the router
         <div className="App">
-            <HomePage />
+            <Router>
+                {/* routes for creating routes and we use routes instead
+                 of switch in v6 */}
+                <Routes>
+                    {/* route to define the route  */}
+                    <Route path="/" exact element={<HomePage />} />
+                    <Route path="/Login" element={<Login />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
