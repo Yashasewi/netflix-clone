e3d99f51739aaf0a63fe81a56122fed7eb167ebf
Yashasewi <Yashasewisingh2668@gmail.com>
2023-06-23 17:45:13 +0530

    ...

--- src/components/Home/Row/Row.jsx
+++ src/components/Home/Row/Row.jsx
@@ -3,46 +3,42 @@ import { useEffect, useState } from "react";
 import axios from "../../../API/axios.js";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
-    const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    // console.log(movies);
-    return (
-        <div className="row">
-            <h2 className="movie_title_row">{title}</h2>
-            <div className="row_posters">
-                {movies.map((movie) => {
-                    return (
-                        <img
-                            key={movie.id}
-                            className={`row__poster ${
-                                isLargeRow && "row__posterLarge"
-                            }`}
-                            alt={movie.name}    
-                            src={`${BASE_URL}${
-                                isLargeRow
-                                    ? movie.poster_path
-                                    : movie.backdrop_path
-                                }`}
-                            onClick={() => {
-                                console.log("yash")
-                            }}
-                        />
-                    );
-                })}
-            </div>
-        </div>
-    );
+  // console.log(movies);
+  return (
+    <div className="row">
+      <h2 className="movie_title_row">{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => {
+          return (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              alt={movie.name}
+              src={`${BASE_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              onClick={() => {
+                console.log();
+              }}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
