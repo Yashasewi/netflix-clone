f0ecc8222de1b7243742fe9edf8c65842fa9e8f2
Yashasewi singh <32950332+Yashasewi@users.noreply.github.com>
2023-07-30 12:15:54 +0530

    bug fixing

--- src/components/Home/NavBar/Nav.jsx
+++ src/components/Home/NavBar/Nav.jsx
@@ -1,4 +1,4 @@
-import {  useEffect } from "react";
+import { useState, useEffect } from "react";
 import netflix from "./netflix.png";
 import "./nav.css";
 import { Link } from "react-router-dom";
